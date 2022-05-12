d9d1cc33cdd98fa5845dee135988525e11854e33
Ryan <ryanrana_4@hotmail.com>
2022-05-13 00:31:23 +0100

    Added quantity checker for checkout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
@@ -8,13 +8,38 @@ import "./App.css";
 function App() {
   const [cart, setCart] = useState([]);
 
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
+  const addtoCart = adding => {
+    cart.includes(adding)
+      ? changeQuantity(adding)
+      : setCart(oldArray => [...oldArray, adding]);
+  };
+
+  const cartQuantity = () => {
+    return cart.reduce((total, cart) => cart.quantity + total, 0);
+  };
+
+  const changeQuantity = adding => {
+    let newCart = [...cart];
+    // wont work but how i left it lol
+    newCart[adding.id - 1].quantity++;
+
+    setCart(newCart);
+  };
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar cart={cart} />
+        <Navbar cartQuantity={cartQuantity} />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/Shop" element={<Shop />} />
+          <Route
+            path="/Shop"
+            element={<Shop cart={cart} addtoCart={addtoCart} />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,12 +1,16 @@
 function Card(props) {
-  const { products } = props;
+  const { products, addProduct } = props;
   return (
     <div className="product">
       {products.map(products => {
         return [
-          <div className="card">
-            <h1 key={products.id}>{products.band}</h1>
+          <div className="card" key={products.id}>
+            <h1>{products.band}</h1>
             <p> Price: £{products.price}</p>
+            <button onClick={e => addProduct(products.id, e)}>
+              {" "}
+              Add to cart
+            </button>
           </div>,
         ];
       })}
