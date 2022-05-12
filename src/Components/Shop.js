b26ed29e4bc7f214af763dfc557f1b2557d10d68
Ryan <ryanrana_4@hotmail.com>
2022-05-13 00:32:26 +0100

    added 3 products that can be put into cart

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,30 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Card from "./Card";
-function Shop() {
+function Shop(props) {
+  const { addtoCart } = props;
   const [products, setProducts] = useState([
     {
       id: 1,
       price: 50,
       band: "blink182",
       album: "Untitled",
+      quantity: 1,
     },
     {
       id: 2,
       price: 50,
       band: "yellowcard",
       album: "ocean avenue",
+      quantity: 1,
     },
     {
       id: 3,
       price: 50,
       band: "Green day",
       album: "Dookie",
+      quantity: 1,
     },
   ]);
 
+  const addProduct = (id, e) => {
+    const currentProduct = products.find(products => products.id === id);
+
+    addtoCart(currentProduct);
+  };
+
   return (
     <div>
-      <Card products={products} />
+      <Card products={products} addProduct={addProduct} />
     </div>
   );
 }
