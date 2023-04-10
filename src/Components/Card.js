53acc472d7cb492d66f5e10398bf5e5cc76486a2
Ryan <ryanrana_4@hotmail.com>
2023-04-10 04:09:26 +0100

    turned Card into bootstrap Card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,23 +1,33 @@
 import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-function Card(props) {
+function ProductCard(props) {
   const { products, addProduct } = props;
   return (
     <div className="product">
       {products.map(products => {
         return [
-          <div className="card" key={products.id}>
-            <h1>{products.band}</h1>
-            <p> Price: £{products.price}</p>
-            <Button variant="dark" onClick={e => addProduct(products.id, e)}>
-              {" "}
-              Add to cart
-            </Button>
-          </div>,
+          <Card border="primary">
+            <div className="card" key={products.id}>
+              <Card.Img /*variant="top" src="holder.js/100px180"*/ />
+              <Card.Title>{products.band}</Card.Title>
+              <Card.Text> Price: £{products.price}</Card.Text>
+              <div>
+                <Button>+</Button>
+                <label className="quantityLabel">0</label>
+                <Button>-</Button>
+              </div>
+              <Button variant="dark" onClick={e => addProduct(products.id, e)}>
+                {" "}
+                Add to cart
+              </Button>
+            </div>
+            <br />
+          </Card>,
         ];
       })}
     </div>
   );
 }
 
-export default Card;
+export default ProductCard;
