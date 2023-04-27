e9ed81f71b081a07daa899d68da89b7667ba3294
Ryan <ryanrana_4@hotmail.com>
2023-04-28 00:24:31 +0100

    Refactored checkout button, deleted dupe code. Code much much cleaner

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -1,70 +1,55 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import basket from "./utils/basket.png";
-
+import Offcanvas from "react-bootstrap/Offcanvas";
 function Checkout(props) {
   const { cart, cartQuantity, list, removeFromBasket } = props;
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const handleClick = e => {
     alert("BBB");
   };
 
-  if (!list) {
-    return (
+  return (
+    <>
       <div className="checkout">
         <img
           src={basket}
-          onClick={handleClick}
+          onClick={handleShow}
           className="basket"
           alt="Basket"
         />
         <div className="quantity">{cartQuantity}</div>
       </div>
-    );
-  } else if (cart.length === 0) {
-    return (
-      <div className="dropdwn">
-        <div className="checkout">
-          <img
-            src={basket}
-            className="basket"
-            onClick={handleClick}
-            alt="Basket"
-          />
-          <div className="quantity">{cartQuantity}</div>
+      {list === true && cart.length === 0 ? (
+        <div className="dropdwn">
+          <div className="dropdwn-content">
+            <p>Basket is empty</p>
+          </div>
         </div>
-        <div className="dropdwn-content">
-          <p>Basket is empty </p>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="dropdwn">
-        <div className="checkout">
-          <img
-            src={basket}
-            onClick={handleClick}
-            className="basket"
-            alt="Basket"
-          />
-          <div className="quantity">{cartQuantity}</div>
-        </div>
-        <div className="dropdwn-content">
-          Your basket:
-          <ul>
-            {cart.map(({ id, album, quantity, price, band }) => (
-              <li key={id}>
-                <h3>{band} </h3>
-                {album} {quantity} £{quantity * price}{" "}
-                <button onClick={e => removeFromBasket({ id }, e)}>X</button>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    );
-  }
+      ) : (
+        list === true &&
+        cart.length > 0 && (
+          <div className="dropdwn-content">
+            Your basket:
+            <ul>
+              {cart.map(({ id, album, quantity, price, band }) => (
+                <li key={id}>
+                  <h3>{band}</h3>
+                  {album} {quantity} £{quantity * price}{" "}
+                  <button onClick={e => removeFromBasket({ id }, e)}>X</button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )
+      )}
+    </>
+  );
 }
 
 export default Checkout;
